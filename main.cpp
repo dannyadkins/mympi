e4dd2181ab5d462159e0207eb6a68ec0be7e1921
danielpadkins <danielpadk@gmail.com>
2024-01-05 15:52:38 -0500

    refactor to custom socket class with cleanup method

--- main.cpp
+++ main.cpp
@@ -3,34 +3,58 @@
 #include <netinet/in.h>
 #include <unistd.h>
 
-int get_available_port(int& sockfd) {
-    sockaddr_in addr;
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    addr.sin_port = 0;  // this allows the OS to choose whatever port is available
-
-    if (bind(sockfd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
-        perror("bind failed");
-        return -1;
+class Socket {
+    int sockfd;
+public:
+    Socket() {
+        sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        if (sockfd < 0) {
+            perror("socket failed");
+            exit(1);
+        }
     }
 
-    socklen_t len = sizeof(addr);
-    if (getsockname(sockfd, (struct sockaddr*)&addr, &len) < 0) {
-        perror("getsockname failed");
-        return -1;
+    int get_port() {
+        sockaddr_in addr;
+        socklen_t len = sizeof(addr);
+        if (getsockname(sockfd, (struct sockaddr*)&addr, &len) < 0) {
+            perror("getsockname failed");
+            return -1;
+        }
+
+        return ntohs(addr.sin_port);
     }
 
-    return ntohs(addr.sin_port);
-}
+    // destructor for cleanup 
+    ~Socket() {
+        std::cout << "socket destroyed on port " << get_port() << std::endl;
+        close(sockfd);
+    }
+
+    int get_available_port() {
+        sockaddr_in addr;
+        addr.sin_family = AF_INET;
+        addr.sin_addr.s_addr = htonl(INADDR_ANY);
+        addr.sin_port = 0;  // this allows the OS to choose whatever port is available
+
+        if (bind(sockfd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+            perror("bind failed");
+            return -1;
+        }
+
+        socklen_t len = sizeof(addr);
+        if (getsockname(sockfd, (struct sockaddr*)&addr, &len) < 0) {
+            perror("getsockname failed");
+            return -1;
+        }
+
+        return ntohs(addr.sin_port);
+    }
+};
 
 int main() {
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) {
-        perror("socket failed");
-        return 1;
-    }
-
-    int port = get_available_port(sockfd);
+    Socket socket;
+    int port = socket.get_available_port();
     
     if (port < 0) {
         return 1;  // if no port available 
@@ -38,7 +62,5 @@ int main() {
 
     std::cout << "Server running on port " << port << std::endl;
 
-    close(sockfd);
     return 0;
 }
-
